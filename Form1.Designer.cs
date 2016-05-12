809c4f7c49361a76bcf4e59d3c687064e87c72b7
hpcslag <cslag@hotmail.com.tw>
2016-05-12 14:57:43 +0800

    VS2015 Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace UploadClientForStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.FullName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PhoneNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SchoolComboBox = new System.Windows.Forms.ComboBox();
+            this.ClassName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_file_path = new System.Windows.Forms.Label();
+            this.UploadFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog_ZIP = new System.Windows.Forms.OpenFileDialog();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(89, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            // 
+            // FullName
+            // 
+            this.FullName.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FullName.Location = new System.Drawing.Point(134, 44);
+            this.FullName.Name = "FullName";
+            this.FullName.Size = new System.Drawing.Size(100, 23);
+            this.FullName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(6, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Fotech CSIE - Student Visit Client Uploader";
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.PhoneNumber.Location = new System.Drawing.Point(134, 77);
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.Size = new System.Drawing.Size(100, 23);
+            this.PhoneNumber.TabIndex = 4;
+            this.PhoneNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress_Only_Number);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(34, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "電話(或手機)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(89, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "學校";
+            // 
+            // SchoolComboBox
+            // 
+            this.SchoolComboBox.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.SchoolComboBox.FormattingEnabled = true;
+            this.SchoolComboBox.Items.AddRange(new object[] {
+            "中庄國中",
+            "大寮國中"});
+            this.SchoolComboBox.Location = new System.Drawing.Point(134, 107);
+            this.SchoolComboBox.Name = "SchoolComboBox";
+            this.SchoolComboBox.Size = new System.Drawing.Size(100, 24);
+            this.SchoolComboBox.TabIndex = 8;
+            // 
+            // ClassName
+            // 
+            this.ClassName.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ClassName.Location = new System.Drawing.Point(134, 142);
+            this.ClassName.Name = "ClassName";
+            this.ClassName.Size = new System.Drawing.Size(100, 23);
+            this.ClassName.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(5, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "班級 (Ex: 3年1班)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(15, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "選擇檔案";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.Location = new System.Drawing.Point(12, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "選擇要上傳的檔案";
+            // 
+            // label_file_path
+            // 
+            this.label_file_path.AutoSize = true;
+            this.label_file_path.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_file_path.Location = new System.Drawing.Point(98, 208);
+            this.label_file_path.Name = "label_file_path";
+            this.label_file_path.Size = new System.Drawing.Size(80, 16);
+            this.label_file_path.TabIndex = 13;
+            this.label_file_path.Text = "尚未選擇檔案";
+            // 
+            // UploadFileButton
+            // 
+            this.UploadFileButton.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.UploadFileButton.ForeColor = System.Drawing.Color.Crimson;
+            this.UploadFileButton.Location = new System.Drawing.Point(9, 249);
+            this.UploadFileButton.Name = "UploadFileButton";
+            this.UploadFileButton.Size = new System.Drawing.Size(262, 34);
+            this.UploadFileButton.TabIndex = 14;
+            this.UploadFileButton.Text = "上傳檔案";
+            this.UploadFileButton.UseVisualStyleBackColor = true;
+            this.UploadFileButton.Click += new System.EventHandler(this.UploadFileClick);
+            // 
+            // openFileDialog_ZIP
+            // 
+            this.openFileDialog_ZIP.FileName = "openFileDialog1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label7.Location = new System.Drawing.Point(236, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 11);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "(含區碼)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 295);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.UploadFileButton);
+            this.Controls.Add(this.label_file_path);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ClassName);
+            this.Controls.Add(this.SchoolComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PhoneNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FullName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FotechCSIE - Student Visit Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FullName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PhoneNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox SchoolComboBox;
+        private System.Windows.Forms.TextBox ClassName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label_file_path;
+        private System.Windows.Forms.Button UploadFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_ZIP;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows; // Application
+
+namespace UploadClientForStudent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SchoolComboBox.SelectedIndex = 0; //To Selected "中庄國中"
+
+            //file selected filter
+            openFileDialog_ZIP.Filter = "Compress File(*.zip;*.rar)|*.zip;*.rar;";
+            openFileDialog_ZIP.FilterIndex = 1;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        public string filePath = @"";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog_ZIP.FileName = "";
+            if (openFileDialog_ZIP.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog_ZIP.FileName;
+                //change text in form
+                label_file_path.Text = filePath;
+            }
+        }
+
+        private bool checkFormIsDone()
+        {
+            if (FullName.Text.Length >= 2 && PhoneNumber.Text.Length >= 9 && ClassName.Text.Length > 2 && filePath.Length > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        private void KeyPress_Only_Number(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void UploadFileClick(object sender, EventArgs e)
+        {
+            if (checkFormIsDone())
+            {
+                MessageBox.Show("進行上傳");
+            }
+            else
+            {
+                MessageBox.Show("表單尚未完成!");
+            }
+        }
+    }
+}
